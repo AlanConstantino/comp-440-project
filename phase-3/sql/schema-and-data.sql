51daea5ab0a6a43df985d2fdf83e110b9f8bf07c
AlanConstantino <38880866+AlanConstantino@users.noreply.github.com>
2022-05-06 03:44:34 -0700

    Updated db schema and data sql file

--- phase-3/sql/schema-and-data.sql
+++ phase-3/sql/schema-and-data.sql
@@ -18,6 +18,7 @@
 --
 -- Table structure for table `blog`
 --
+use `comp440`;
 
 DROP TABLE IF EXISTS `blog`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -32,7 +33,7 @@ CREATE TABLE `blog` (
   UNIQUE KEY `idBlog_UNIQUE` (`idBlog`),
   KEY `fk_idUser_idx` (`idUser`),
   CONSTRAINT `fk_idUser` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +42,7 @@ CREATE TABLE `blog` (
 
 LOCK TABLES `blog` WRITE;
 /*!40000 ALTER TABLE `blog` DISABLE KEYS */;
-INSERT INTO `blog` VALUES (1,1,'My first post ever!','My first ever description!','2022-05-01 00:00:00'),(18,7,'Hello I\'m Yosef!','I\'m introducing myself! Hello!','2022-05-01 00:00:00'),(19,1,'I like this website!','This webapp is really cool.','2022-04-20 00:00:00'),(20,3,'Snakes','Venomous snakes are my favorite reptiles.','2022-04-20 00:00:00'),(21,3,'New Phone','Should I get a new phone? Let me know in the comments guys!','2022-04-20 00:00:00'),(22,5,'Vinyl Music','I am a collector of vinyls.','2022-05-01 00:00:00'),(23,1,'Discord','Let me know in the comments if anyone uses discord.','2022-04-20 00:00:00'),(24,12,'Youtube Competitor','Someone should really make a webapp that competes with YouTube.','2022-04-24 00:00:00'),(25,1,'This is my important subject','My description!','2022-05-01 00:00:00'),(26,12,'This is another post!','This is a description','2022-04-25 00:00:00'),(27,9,'Rock music rocks!','I really like rock music!','2022-05-05 00:00:00'),(28,5,'Rap music rocks!','I really like rap music!','2022-05-05 00:00:00'),(29,11,'Really important post','Really important description.','2022-05-05 00:00:00'),(30,7,'Cakes are great!','I love cakes! They\'re easy to make and are delicious!','2022-05-05 00:00:00'),(31,7,'I hate running :(','Running hurts my feet.','2022-05-05 00:00:00'),(32,8,'My subject.','My description.','2022-05-05 00:00:00'),(33,3,'SQL is easy','SQL is pretty easy to learn, just saying.','2022-05-05 00:00:00'),(34,3,'Why is the sky blue?','I wonder why the sky is blue. Can someone explain why?','2022-05-05 00:00:00'),(35,12,'What kind of car do you drive?','I drive a Tesla model 3, what about you guys?','2022-05-05 00:00:00');
+INSERT INTO `blog` VALUES (1,1,'My first post ever!','My first ever description!','2022-05-01 00:00:00'),(18,7,'Hello I\'m Yosef!','I\'m introducing myself! Hello!','2022-05-01 00:00:00'),(19,1,'I like this website!','This webapp is really cool.','2022-04-20 00:00:00'),(20,3,'Snakes','Venomous snakes are my favorite reptiles.','2022-04-20 00:00:00'),(21,3,'New Phone','Should I get a new phone? Let me know in the comments guys!','2022-04-20 00:00:00'),(22,5,'Vinyl Music','I am a collector of vinyls.','2022-05-01 00:00:00'),(23,1,'Discord','Let me know in the comments if anyone uses discord.','2022-04-20 00:00:00'),(24,12,'Youtube Competitor','Someone should really make a webapp that competes with YouTube.','2022-04-24 00:00:00'),(25,1,'This is my important subject','My description!','2022-05-01 00:00:00'),(26,12,'This is another post!','This is a description','2022-04-25 00:00:00'),(27,9,'Rock music rocks!','I really like rock music!','2022-05-05 00:00:00'),(28,5,'Rap music rocks!','I really like rap music!','2022-05-05 00:00:00'),(29,11,'Really important post','Really important description.','2022-05-05 00:00:00'),(30,7,'Cakes are great!','I love cakes! They\'re easy to make and are delicious!','2022-05-05 00:00:00'),(31,7,'I hate running :(','Running hurts my feet.','2022-05-05 00:00:00'),(32,8,'My subject.','My description.','2022-05-05 00:00:00'),(33,3,'SQL is easy','SQL is pretty easy to learn, just saying.','2022-05-05 00:00:00'),(34,3,'Why is the sky blue?','I wonder why the sky is blue. Can someone explain why?','2022-05-05 00:00:00'),(35,12,'What kind of car do you drive?','I drive a Tesla model 3, what about you guys?','2022-05-05 00:00:00'),(36,15,'Hello I\'m Carina!','This is my first post. Hello to all!','2022-05-05 00:00:00'),(37,15,'Help With Homework','I need help with my homework please.','2022-05-05 00:00:00');
 /*!40000 ALTER TABLE `blog` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -95,7 +96,7 @@ CREATE TABLE `follow` (
   KEY `fk_follower_idx` (`idFollower`),
   CONSTRAINT `fk_follow` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`),
   CONSTRAINT `fk_follower` FOREIGN KEY (`idFollower`) REFERENCES `user` (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,7 +105,7 @@ CREATE TABLE `follow` (
 
 LOCK TABLES `follow` WRITE;
 /*!40000 ALTER TABLE `follow` DISABLE KEYS */;
-INSERT INTO `follow` VALUES (34,3,7),(35,3,12),(36,4,12),(37,4,7),(38,4,11),(39,8,4),(40,8,12),(41,3,1),(42,12,1),(43,15,11),(44,15,12),(45,15,13),(46,15,14),(47,7,15);
+INSERT INTO `follow` VALUES (34,3,7),(35,3,12),(36,4,12),(37,4,7),(38,4,11),(39,8,4),(40,8,12),(41,3,1),(42,12,1),(43,15,11),(44,15,12),(45,15,13),(46,15,14),(47,7,15),(48,1,15),(49,3,15);
 /*!40000 ALTER TABLE `follow` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,7 +124,7 @@ CREATE TABLE `tag` (
   UNIQUE KEY `idtags_UNIQUE` (`idtag`),
   KEY `idBlog_idx` (`idBlog`),
   CONSTRAINT `idBlog` FOREIGN KEY (`idBlog`) REFERENCES `blog` (`idBlog`)
-) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,7 +133,7 @@ CREATE TABLE `tag` (
 
 LOCK TABLES `tag` WRITE;
 /*!40000 ALTER TABLE `tag` DISABLE KEYS */;
-INSERT INTO `tag` VALUES (7,'cars',18),(8,'abc',19),(9,'gef',19),(10,'ssfwa',19),(11,'sssss',20),(12,'apple',21),(13,'orange',21),(14,'apple',23),(15,'orange',23),(16,'banana',23),(17,'apple',24),(18,'orange',24),(19,'banana',24),(20,'kiwi',24),(21,'apple',25),(22,'orange',25),(23,'banana',25),(24,'tags1',26),(25,'tags2',26),(26,'tags3',26),(27,'house',22),(28,'69',27),(29,'1',28),(30,'mytag apple',29),(31,'a',30),(32,'p',30),(33,'p',30),(34,'l',30),(35,'e',30),(36,'o',31),(37,'r',31),(38,'a',31),(39,'n',31),(40,'g',31),(41,'e',31),(42,'k',32),(43,'i',32),(44,'w',32),(45,'i',32),(46,'k',33),(47,'i',33),(48,'w',33),(49,'i',33),(50,'s',34),(51,'t',34),(52,'r',34),(53,'a',34),(54,'w',34),(55,'b',34),(56,'e',34),(57,'r',34),(58,'r',34),(59,'y',34),(60,'b',35),(61,'a',35),(62,'n',35),(63,'a',35),(64,'n',35),(65,'a',35);
+INSERT INTO `tag` VALUES (7,'cars',18),(8,'flight',19),(9,'apple',19),(10,'bike',19),(11,'kiwi',20),(12,'apple',21),(13,'orange',21),(14,'apple',23),(15,'orange',23),(16,'banana',23),(17,'apple',24),(18,'orange',24),(19,'banana',24),(20,'kiwi',24),(21,'apple',25),(22,'orange',25),(23,'banana',25),(24,'bike',26),(25,'motorcycle',26),(26,'dance',26),(27,'house',22),(28,'mysterious',27),(29,'one',28),(30,'funny',29),(31,'ape',30),(42,'karate',32),(50,'strawberry',34),(60,'bunny',35),(66,'new',36),(67,'hello',36),(68,'first',36),(69,'homework',37),(70,'slow',33),(71,'exercise',31),(72,'tired',31),(73,'first',1),(74,'new',1);
 /*!40000 ALTER TABLE `tag` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -177,4 +178,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-05 23:23:02
+-- Dump completed on 2022-05-06  3:42:22
